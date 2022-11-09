608e8b46df155d825cd6781426a4b3e806b4af3e
Krypton Kat <binitme449@gmail.com>
2022-11-09 10:01:14 +0545

    3

--- src/Pages/index.js
+++ src/Pages/index.js
@@ -1,3 +1,10 @@
 import HomePage from "./Home.Page";
+import About from "./About.Page";
+import Categories from "./Categories.Page";
+import Contact from "./Contact.Page";
+import Error from "./Error.Page";
+import Login from "./Login.Page";
+import News from "./News.Page";
+import Register from "./Register.Page";
 
-export { HomePage };
+export { HomePage, About, Categories, Contact, Error, Login, News, Register };
